3169ca88a0aa8771378713060ea9dfef79d9ef99
Diego Rondão <diegorondao86@gmail.com>
2023-03-02 12:29:07 -0300

    feat: implementado recurso de customer (#7)
    plt-139

---
+++ src/Types/Customer.ts
@@ -0,0 +1,22 @@
+
+interface Company {
+  name: string;
+  cnpj?: string;
+}
+
+export interface Create {
+  name: string;
+  company: Company;
+  occupation_id: string[];
+  service_providers: string[];
+}
+
+export interface Update {
+  name: string;
+  company: Company;
+}
+
+export interface UpdateOcccupations {
+  alias: string;
+}
+
