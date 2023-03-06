361195041cc88ac6896094c790003d45de4875f8
Diego Rondao <diegorondao86@gmail.com>
2023-03-06 10:57:40 -0300

    feat: add class, type and test to Weather

---
+++ src/Types/Weather.ts
@@ -0,0 +1,14 @@
+export interface Create {
+  construction_id: string;
+  period: string;
+  weather: string;
+  condition: string;
+  description: string;
+}
+export interface Update {
+  construction_id: string;
+  period: string;
+  weather: string;
+  condition: string;
+  description: string;
+}
